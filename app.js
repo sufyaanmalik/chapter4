bcc18059b15a405bbf530a8fb687f092886628b6
sufyaanmalik <sufyaanmaalik@gmail.com>
2024-12-22 01:10:15 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Chapter 4
+
+// Question # 1
+
+let name = "Sufyan", age = 24, isStudent = true;
+
+
+// Question # 2
+
+// 5 Legal Variable Names
+
+let userName;
+let _age;
+let $price;
+let studentCount;
+let total5;
+
+// 5 Illegal Variable Names
+
+let 1name;
+let user-name;
+let var;
+let user name;
+let @price;
